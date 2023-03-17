d223136cbeef38ea9e9d05c6a84f7ddaedd30cd0
melisand <melisandoval@live.com.ar>
2023-03-17 18:04:24 +0100

    add comments and clean code

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Square } from "./components/Square";
 import { PLAYERS } from "./constants";
 
 const initialBoard = Array(9).fill(null);
-const { playerOne, playerTwo } = PLAYERS;
+const { playerOne, playerTwo } = PLAYERS; // destructuring the two players
 
 function App() {
   const [board, setBoard] = useState(initialBoard);
@@ -12,8 +12,10 @@ function App() {
 
   const updateBoard = (cellNumber) => {
     const newBoard = [...board];
-    newBoard[cellNumber] = turn;
-    setBoard(newBoard);
+    newBoard[cellNumber] = turn; // turn can be playerOne or playerTwo.
+    // cellNumber is the same as the index of the element in the array newBoard,
+    // so with this we update that specific cell/index with playerOne or playerTwo
+    setBoard(newBoard); // now we update the UI board with the updated board
 
     const newTurn = turn === playerOne ? playerTwo : playerOne;
     setTurn(newTurn);
@@ -36,12 +38,8 @@ function App() {
       <section className="turn-section">
         <h2>Turn:</h2>
         <div className="turn">
-          <Square isSelected={turn === PLAYERS.playerOne}>
-            {PLAYERS.playerOne}
-          </Square>
-          <Square isSelected={turn === PLAYERS.playerTwo}>
-            {PLAYERS.playerTwo}
-          </Square>
+          <Square isSelected={turn === playerOne}>{playerOne}</Square>
+          <Square isSelected={turn === playerTwo}>{playerTwo}</Square>
         </div>
       </section>
     </main>
