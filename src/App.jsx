2c6b3105b052aed1249240abc4fad6647f2dbf68
melisand <melisandoval@live.com.ar>
2023-03-17 18:34:05 +0100

    update the function updateBoard and add comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,36 @@ const { playerOne, playerTwo } = PLAYERS; // destructuring the two players
 
 function App() {
   const [board, setBoard] = useState(initialBoard);
-  const [turn, setTurn] = useState(playerOne);
+  const [turn, setTurn] = useState(playerOne); // turn can be playerOne or playerTwo.
+
+  // this function is called everytime a cell is clicked:
 
   const updateBoard = (cellNumber) => {
+    // (cellNumber is the same as the index of the array "board")
+
+    // Prevent from updating the board if the clicked cell already has a value:
+    if (board[cellNumber]) return;
+    // (if the element in this position of the array board is true: return)
+    // (the default of the cell is null, so if now already has value is because a player already clicked the cell before)
+    // We should prevent any of the two players from changing the cell value again,
+    // and this is easy to do with just an early return of this function.
+
     const newBoard = [...board];
-    newBoard[cellNumber] = turn; // turn can be playerOne or playerTwo.
+    // ↑ copying the previous board state because we never have to mutate the state
+    // if the state is an array, we should never change it. We have to always create a new array,
+    // and then use the "setState" function.
+    // If we update the array of the state "board" we will be updating the state without calling
+    // "setBoard", and that could cause discrepancies in the rendering
+    // the data of the new state always has to be new.
+
+    newBoard[cellNumber] = turn; // state turn can be playerOne or playerTwo.
     // cellNumber is the same as the index of the element in the array newBoard,
     // so with this we update that specific cell/index with playerOne or playerTwo
+    // if necessary, instead of spread operator we can use structuredClone()
+
     setBoard(newBoard); // now we update the UI board with the updated board
 
+    // update the turn:
     const newTurn = turn === playerOne ? playerTwo : playerOne;
     setTurn(newTurn);
   };
